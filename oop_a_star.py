50f25d9a2bddc8b53a3e42a4471f5421673f5e7e
Anja Sheppard <sheppard18a@ncssm.edu>
2017-10-03 15:15:22 -0400

    some bugs fixed, but neighbor stuff not working

--- oop_a_star.py
+++ oop_a_star.py
@@ -4,24 +4,14 @@ import sys
 
 from ast import literal_eval as make_tuple
 
-map_img = cv2.imread("test.png")
-map_img = cv2.cvtColor(map_img,cv2.COLOR_RGB2GRAY)
-home = make_tuple(sys.argv[1])
-goal = make_tuple(sys.argv[2])
-step = 0.2 # m
-
-print(map_img)
-
-print(home, goal)
-
 class Node:
 
     def __init__ (self, home=(0,0), goal=(1,1), location=(0,0)):
-        self.location
+        self.location = location
         self.home = home
         self.goal = goal
         self.neighbors = []
-        self.gScore = self.gScore(self.home, self.location)
+        self.gScore = self.gScore()
         self.hScore = self.hScore()
         self.fScore = self.gScore + self.hScore
 
@@ -33,23 +23,31 @@ class Node:
         manhattan_dist = (self.goal[1] - self.location[1]) + (self.goal[0] - self.location[0])
         return manhattan_dist
 
-    def generateNeighbors ():
+    def generateNeighbors (self):
         neighbors = []
-        if self.location[0] + 1 < len(map_img[0]):
+        print(map_img[self.location[0]][self.location[1] + 1])
+        if self.location[0] + 1 < len(map_img[0]) and map_img[self.location[0] + 1][self.location[1]] is not 0:
             neighbors.append(Node(self.home, self.goal, (self.location[0] + 1, self.location[1])))
-        if self.location[0] - 1 >= 0:
+        if self.location[0] - 1 >= 0 and map_img[self.location[0] - 1][self.location[1]] is not 0:
             neighbors.append(Node(self.home, self.goal, (self.location[0] - 1, self.location[1])))
-        if self.location[1] + 1 < len(map_img)
+        if self.location[1] + 1 < len(map_img) and map_img[self.location[0]][self.location[1] + 1] is not 0:
             neighbors.append(Node(self.home, self.goal, (self.location[0], self.location[1] + 1)))
-        if self.location[1] - 1 >= 0:
-            neighbors.append(Node(self.home, self.goal, (self.location[0] self.location[1] - 1)))
+        if self.location[1] - 1 >= 0 and map_img[self.location[0]][self.location[1] - 1] is not 0:
+            neighbors.append(Node(self.home, self.goal, (self.location[0], self.location[1] - 1)))
         self.neighbors = neighbors
+
+    def __str__ (self):
+        return "\n----------\n" + "Location: " + str(self.location) + "\nHome: " + str(self.home) + "\nGoal: " + str(self.goal) + "\nNumber of Neighbors: " + str(len(self.neighbors)) + "\nG Score: " + str(self.gScore) + "\nH Score: " + str(self.hScore) + "\nF Score: " + str(self.fScore) + "\n----------\n"
 
 def isNotIn (neighbor, listy):
     for i in listy:
         if neighbor is i:
-            return True
-    return False
+            return False
+    return True
+
+def printMap ((index1, index2)):
+    map_img[index1, index2] = 999
+    print(map_img)
 
 def a_star ():
     # create the fringe and alreadyVisited lists
@@ -62,21 +60,38 @@ def a_star ():
 
     # add home as first element of fringe
     fringe.append(home_node)
-    listOfActions.append(home_node.location)
-
-    while not fringe.empty():
+    listOfActions.append(home_node)
+    while len(fringe) != 0:
         cur_node = listOfActions[-1] # expand on last best node
         if cur_node.location == goal:
             print("Made it to the goal!")
             return listOfActions
         cur_node.generateNeighbors() # make new nodes to check
         for neighbor in cur_node.neighbors:
-            if neightbor isNotIn(neighbor, alreadyVisited): # make sure you haven't already checked that node
+            if isNotIn(neighbor, alreadyVisited): # make sure you haven't already checked that node
                 fringe.append(neighbor)
+                cur_node.neighbors.pop(0)
             if neighbor.fScore > cur_node.fScore: # if score is higher, this path is not better thusc continue without saving this node
                 alreadyVisited.append(neighbor)
                 break
             listOfActions.append(neighbor) # if score is better, save this as part of your path
+        fringe.pop()
+        print(cur_node)
+        printMap(cur_node.location)
 
+if __name__ == "__main__":
 
-aStar()
+    map_img = cv2.imread("test.png")
+    map_img = cv2.cvtColor(map_img,cv2.COLOR_RGB2GRAY)
+    if make_tuple(sys.argv[2])[0] < len(map_img) and make_tuple(sys.argv[2])[0] < len(map_img[0]):
+        home = make_tuple(sys.argv[1])
+        goal = make_tuple(sys.argv[2])
+    else:
+        print("Goal out of bounds. Please run again.")
+    step = 0.2 # m
+
+    print(map_img)
+
+    print(home, goal)
+
+    a_star()
